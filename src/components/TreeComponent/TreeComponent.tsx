ebded5ce0bbf0ecd0d0a47ceeef6bad4b2a8430c
Ralvarez <R.alvgar@hotmail.com>
2020-11-27 09:15:01 +0100

    changes on tsconfing

--- src/components/TreeComponent/TreeComponent.tsx
+++ src/components/TreeComponent/TreeComponent.tsx
@@ -1,20 +1,18 @@
-import React, { useState,useMemo } from 'react';
+import React, { useState } from 'react';
 import './TreeComponent.scss';
 
 const TreeComponent = ({ file }: any) => {
     const [isVisible, setIsVisible] = useState(true);
 
-    const paco = useMemo(() => 
-    (file.folder !== undefined) && file.folder.map((value: any, index: any) =>
-    <li key={value + index} style={!isVisible ? {visibility: "hidden"} : {display: "inline"}} ><TreeComponent file={value} />{value.name}</li>)
-    , [file.folder]);
 
     return (
-        
-            <ul className='list-unstyled'>
-                {paco}
-            </ul>
-        
+
+        <ul className='list-unstyled'>
+            {Object.keys(file).map((key: any, idx: any) => (key === 'name') ?
+            <li key={key + idx} style={!isVisible ? { visibility: "hidden" } : { display: "inline" }} >{file[key]}</li>:
+            <TreeComponent file={file[key]} />)}
+        </ul>
+
     )
 }
 
